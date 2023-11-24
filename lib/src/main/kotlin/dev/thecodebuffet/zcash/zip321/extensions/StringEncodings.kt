35a33cd66e33084dcf0612127bd33eb25e1543b5
Francisco Gindre <francisco.gindre@gmail.com>
2023-11-24 16:48:08 -0300

    [#13] qchar encoding Tests

--- lib/src/main/kotlin/dev/thecodebuffet/zcash/zip321/extensions/StringEncodings.kt
+++ lib/src/main/kotlin/dev/thecodebuffet/zcash/zip321/extensions/StringEncodings.kt
@@ -3,6 +3,6 @@ package dev.thecodebuffet.zcash.zip321.extensions
 fun String.qcharEncoded(): String? {
     val qcharEncodeAllowed = setOf('-', '.', '_', '~', '!', '$', '\'', '(', ')', '*', '+', ',', ';', '@', ':').map { it -> it.toString() }
     return this.replace(Regex("[^A-Za-z0-9\\-._~!$'()*+,;@:]")) { matched ->
-        if (matched.value in qcharEncodeAllowed) matched.value else "%" + matched.value.toCharArray().joinToString("%") { byte -> "%02x".format(byte.code.toByte()) }
+        if (matched.value in qcharEncodeAllowed) matched.value else "%" + matched.value.toCharArray().joinToString("%") { byte -> "%02X".format(byte.code.toByte()) }
     }
 }
---
+++ lib/src/test/kotlin/dev/thecodebuffet/zcash/zip321/EncodingTests.kt
@@ -0,0 +1,65 @@
+package dev.thecodebuffet.zcash.zip321
+
+import dev.thecodebuffet.zcash.zip321.extensions.qcharEncoded
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+
+
+class EncodingTests : FunSpec({
+
+    test("qcharEncoded string contains allowed characters only") {
+        val message = "sk8:forever@!"
+        message.qcharEncoded() shouldBe message
+    }
+
+    test("qcharEncoded string has percent-encoded disallowed characters") {
+        mapOf(
+            "Thank you for your purchase" to "Thank%20you%20for%20your%20purchase",
+            "Use Coupon [ZEC4LIFE] to get a 20% discount on your next purchase!!" to
+                    "Use%20Coupon%20%5BZEC4LIFE%5D%20to%20get%20a%2020%25%20discount%20on%20your%20next%20purchase!!",
+            "Order #321" to "Order%20%23321",
+            "Your Ben & Jerry's Order" to "Your%20Ben%20%26%20Jerry's%20Order",
+            " " to "%20",
+            "\"" to "%22",
+            "#" to "%23",
+            "%" to "%25",
+            "&" to "%26",
+            "/" to "%2F",
+            "<" to "%3C",
+            "=" to "%3D",
+            ">" to "%3E",
+            "?" to "%3F",
+            "[" to "%5B",
+            "\\" to "%5C",
+            "]" to "%5D",
+            "^" to "%5E",
+            "`" to "%60",
+            "{" to "%7B",
+            "|" to "%7C",
+            "}" to "%7D"
+        ).forEach { (input, expected) ->
+            input.qcharEncoded() shouldBe expected
+        }
+    }
+
+    test("unallowed characters are escaped") {
+        val unallowedCharacters = listOf(
+            " ", "\"", "#", "%", "&", "/", "<", "=", ">", "?", "[", "\\", "]", "^", "`", "{", "|", "}"
+        )
+
+        unallowedCharacters.forEach { unallowed ->
+            val qcharEncoded = unallowed.qcharEncoded()
+            qcharEncoded should {
+                it != null && it.contains("%")
+            }
+        }
+
+        (0x00..0x1F).map { it.toChar().toString() }.forEach { controlChar ->
+            val qcharEncoded = controlChar.qcharEncoded()
+            qcharEncoded should {
+                it != null && it.contains("%")
+            }
+        }
+    }
+})
